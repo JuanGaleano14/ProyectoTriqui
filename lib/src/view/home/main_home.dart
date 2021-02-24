fa1c5c047fff01bc85a32768ff6c31e0031c04f7
Juan Pablo Galeano S <56057491+JuanGaleano14@users.noreply.github.com>
2021-02-24 01:48:16 -0500

    Estructuración de carpetas
    Se organiza la estructura de carpetas en base a Vista, Modelo y Bloc. Se añade txt a carpetas vacías.
    
    Se separa la vista del home de la clase main, y se traslada a view/home

---
+++ lib/src/view/home/main_home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Esqueleto
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Has hecho clic en el botón tantas veces:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Aumentar',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
