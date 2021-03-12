3b93b6a23ce5c59687ea5940ddff5c53f8207fd8
Juan Pablo Galeano S <56057491+JuanGaleano14@users.noreply.github.com>
2021-03-11 19:13:18 -0500

    Modificación bloc

--- lib/src/bloc/tablero_bloc.dart
+++ lib/src/bloc/tablero_bloc.dart
@@ -1,3 +1,75 @@
 import 'package:flutter_app_prueba/src/models/tablero_model.dart';
 
-class TableroBloc extends Tablero {}
+class TableroBloc {
+
+  static bool validarGanador(Tablero tablero) {
+    bool ganador;
+    // checks 1st row
+    if (tablero.celdas[0] == tablero.celdas[1] &&
+        tablero.celdas[0] == tablero.celdas[2] &&
+        tablero.celdas[0] != '') {
+      ganador = confirmarGanador(tablero.celdas[0]);
+    }
+
+    // checks 2nd row
+    if (tablero.celdas[3] == tablero.celdas[4] &&
+        tablero.celdas[3] == tablero.celdas[5] &&
+        tablero.celdas[3] != '') {
+      ganador = confirmarGanador(tablero.celdas[3]);
+    }
+
+    // checks 3rd row
+    if (tablero.celdas[6] == tablero.celdas[7] &&
+        tablero.celdas[6] == tablero.celdas[8] &&
+        tablero.celdas[6] != '') {
+      ganador = confirmarGanador(tablero.celdas[6]);
+    }
+
+    // checks 1st column
+    if (tablero.celdas[0] == tablero.celdas[3] &&
+        tablero.celdas[0] == tablero.celdas[6] &&
+        tablero.celdas[0] != '') {
+      ganador = confirmarGanador(tablero.celdas[0]);
+    }
+
+    // checks 2nd column
+    if (tablero.celdas[1] == tablero.celdas[4] &&
+        tablero.celdas[1] == tablero.celdas[7] &&
+        tablero.celdas[1] != '') {
+      ganador = confirmarGanador(tablero.celdas[1]);
+    }
+
+    // checks 3rd column
+    if (tablero.celdas[2] == tablero.celdas[5] &&
+        tablero.celdas[2] == tablero.celdas[8] &&
+        tablero.celdas[2] != '') {
+      ganador = confirmarGanador(tablero.celdas[2]);
+    }
+
+    // checks diagonal der to izq
+    if (tablero.celdas[6] == tablero.celdas[4] &&
+        tablero.celdas[6] == tablero.celdas[2] &&
+        tablero.celdas[6] != '') {
+      ganador = confirmarGanador(tablero.celdas[6]);
+    }
+
+    // checks diagonal izq to der
+    if (tablero.celdas[0] == tablero.celdas[4] &&
+        tablero.celdas[0] == tablero.celdas[8] &&
+        tablero.celdas[0] != '') {
+      ganador = confirmarGanador(tablero.celdas[0]);
+    }
+
+    return ganador;
+  }
+
+  static bool confirmarGanador(String figuraGanador) {
+    bool ganador;
+    if (figuraGanador == 'O' || figuraGanador == 'X') {
+      ganador = true;
+    } else {
+      ganador = false;
+    }
+    return ganador;
+  }
+}
--- test/validar_ganador_test.dart
+++ test/validar_ganador_test.dart
@@ -1,8 +1,18 @@
+import 'package:flutter_app_prueba/src/models/tablero_model.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_app_prueba/src/bloc/tablero_bloc.dart';
 
 void main() {
   test('Jugador Ganador Test', () {
-    //var result = _MyHomePageState._validarGanador;
-    //expect(result, 'Enter Email!');
+    var result = TableroBloc.confirmarGanador('');
+    expect(result, false);
   });
+
+  /*test('validar tablero ganador', () {
+    var tableroTest =
+    Tablero(celdas: ['', '', '', '', '', '', '', '', '']);
+
+    var result = TableroBloc.validarGanador(tableroTest);
+    expect(result, false);
+  });*/
 }
